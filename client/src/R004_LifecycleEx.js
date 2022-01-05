4fc9a48f0a69c1a03f9eb9f5c3af6400f3c749df
최 혜진(Ella) <ella@optolane.com>
2022-01-05 12:43:24 +0900

    생명주기 함수 render() 사용하기

---
+++ client/src/R004_LifecycleEx.js
@@ -0,0 +1,14 @@
+import React, {
+  Component
+} from 'react';
+
+class R004_LifecycleEx extends Component {
+  render() {
+    console.log('3. render Call');
+    return (
+      <h2>[THIS IS RENDER FUNCTION]</h2>
+    )
+  }
+}
+
+export default R004_LifecycleEx;

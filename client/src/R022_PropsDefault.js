83ab65cd261a6e6ed388b95beaa2b2bdce1fb32c
최 혜진(Ella) <ella@optolane.com>
2022-01-13 12:30:24 +0900

    props를 기본값으로 정의하기

--- client/src/R022_PropsDefault.js
+++ client/src/R022_PropsDefault.js
@@ -4,8 +4,21 @@ import React, {
 
 class R022_PropsDefault extends Component {
   render() {
-
+    let {
+      ReactString,
+      ReactNumber
+    } = this.props;
+    return(
+      <div style={{padding: "0px"}}>
+        {ReactString}{ReactNumber}
+      </div>
+    )
   }
+}
+
+R022_PropsDefault.defaultProps = {
+  ReactString: "리액트",
+  ReactNumber: 200
 }
 
 export default R022_PropsDefault;

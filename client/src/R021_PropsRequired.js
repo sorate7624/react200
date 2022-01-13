d0533b02fd232cc795098c27ffefd8abd1d00ff2
최 혜진(Ella) <ella@optolane.com>
2022-01-13 12:27:59 +0900

    props를 필수 값으로 사용하기

--- client/src/R021_PropsRequired.js
+++ client/src/R021_PropsRequired.js
@@ -1,11 +1,24 @@
 import React, {
   Component
 } from 'react';
+import datatype from 'prop-types';
 
 class R021_PropsRequired extends Component {
   render() {
-
+    let {
+      ReactString,
+      ReactNumber
+    } = this.props;
+    return(
+      <div style={{padding: "0px"}}>
+        {ReactString}{ReactNumber}
+      </div>
+    )
   }
+}
+
+R021_PropsRequired.proptotype = {
+  ReactString: datatype.isRequred
 }
 
 export default R021_PropsRequired;

151725c88afff8697eae1b722f4ca4682c9323b8
최 혜진(Ella) <ella@optolane.com>
2022-01-19 12:33:18 +0900

    setState() 함수 사용하기

--- client/src/R025_SetState.js
+++ client/src/R025_SetState.js
@@ -3,8 +3,36 @@ import React, {
 } from 'react';
 
 class R025_SetState extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      StateString: 'react'
+    }
+  }
 
+  StateChange = (flag) => {
+    if (flag == 'direct') {
+      this.state.StateString = '리액트';
+    }
+    if (flag == 'setState') {
+      this.setState({
+        StateString: '리액트'
+      })
+    }
+  }
+
+  render() {
+    return(
+      <div style={{padding: "0px"}}>
+        <button onClick={(e)=>this.StateChange('direct', e)}>
+          state 직접 변경
+        </button>
+        <button onClick={(e)=>this.StateChange('setstate',e)}>
+          setState로 변경
+        </button><br/>
+        [state 변경하기] StateString: {this.state.StateString}
+      </div>
+    )
   }
 }
 

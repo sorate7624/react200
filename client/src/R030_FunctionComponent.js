6310d1b89b24946615eba1d407e6f24903e91d48
최 혜진(Ella) <ella@optolane.com>
2022-01-19 12:39:28 +0900

    함수형 컴포넌트 사용하기

--- client/src/R030_FunctionComponent.js
+++ client/src/R030_FunctionComponent.js
@@ -1,11 +1,13 @@
-import React, {
-  Component
-} from 'react';
+import React from 'react'
 
-class R030_FunctionComponent extends Component {
-  render() {
+function R030_FunctionComponent(props) {
+  let {
+    contents
+  } = props;
 
-  }
+  return (
+    <h2>{contents}</h2>
+  ) 
 }
 
 export default R030_FunctionComponent;

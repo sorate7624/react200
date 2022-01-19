51b9aa43036750c93782a58d95376bd66264c9a8
최 혜진(Ella) <ella@optolane.com>
2022-01-19 12:36:37 +0900

    PureComponent 사용하기(class형 컴포넌트)

--- client/src/R028_PureComponentClass.js
+++ client/src/R028_PureComponentClass.js
@@ -3,8 +3,38 @@ import React, {
 } from 'react';
 
 class R028_PureComponentClass extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      StateString: 'react',
+      StateArrayObj: ['react', {
+        react: '200'
+      }]
+    }
+  }
 
+  buttonClick = (type) => {
+    if (type === 'String') {
+      this.setState({
+        StateString: 'react'
+      });
+    } else {
+      this.setState({
+        StateArrayObj: ['react', {
+          react: '200'
+        }]
+      });
+    }
+  }
+
+  render() {
+    console.log('render() 실행');
+    return (
+      <div>
+        <button onClick={e=>this.buttonClick('String')}>문자열 변경</button>
+        <button onClick={e=>this.buttonClick('ArrayObject')}>객체 배열 변경</button>
+      </div>
+    )
   }
 }
 

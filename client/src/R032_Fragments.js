c94e6a6a027c0e0cd5edd6eea501db44ac5ef2ce
최 혜진(Ella) <ella@optolane.com>
2022-01-24 12:11:16 +0900

    Fragments 사용하기

--- client/src/R032_Fragments.js
+++ client/src/R032_Fragments.js
@@ -3,9 +3,13 @@ import React, {
 } from 'react';
 
 class R032_Fragments extends Component {
-
   render() {
-
+    return (
+      <React.Fragment>
+        <p>P TAG</p>
+        <span>SPAN TAG</span>
+      </React.Fragment>
+    )
   }
 }
 

68b3e2de2811c4b08d465f3998915ff55042fd90
최 혜진(Ella) <ella@optolane.com>
2022-01-24 12:18:36 +0900

    map()으로 element 반환하기

--- client/src/R033_ReturnMap.js
+++ client/src/R033_ReturnMap.js
@@ -3,9 +3,18 @@ import React, {
 } from 'react';
 
 class R033_ReturnMap extends Component {
-
   render() {
-
+    const element_Array = [
+      <li>react</li>, 
+      <li>200</li>, 
+      <li>Array map</li>
+    ];
+    
+    return (
+      <ul>
+        {element_Array.map((array_val) => array_val)}
+      </ul>
+    )
   }
 }
 
